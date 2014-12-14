bce5aa8571c99535b4d32e342d70dfa3369085d8
Nick Wientge <nick@exygeninteractive.com>
2014-12-14 13:49:39 -0800

    initial commit

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'meteoric:ionicons-sass',
+  summary: 'Ionicons SASS bundled for Meteor.',
+  version: '0.1.0',
+  git: 'https://github.com/meteoric/ionicons-sass'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.use('stevezhu:sass@1.3.2');
+  api.addFiles('sass_include_paths.json', 'server');
+
+  api.addFiles('ionicons-fonts/ionicons.eot', 'client');
+  api.addFiles('ionicons-fonts/ionicons.svg', 'client');
+  api.addFiles('ionicons-fonts/ionicons.ttf', 'client');
+  api.addFiles('ionicons-fonts/ionicons.woff', 'client');
+});
