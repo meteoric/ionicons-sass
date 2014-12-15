cc674788c34364a7a55a966b75c9b723c95240d1
Nick Wientge <nick@exygeninteractive.com>
2014-12-14 16:44:41 -0800

    add scss files in package.js

--- package.js
+++ package.js
@@ -1,17 +1,30 @@
 Package.describe({
   name: 'meteoric:ionicons-sass',
   summary: "Ionic's Ionicons library in SASS and bundled for Meteor.",
-  version: '0.1.1',
+  version: '0.1.2',
   git: 'https://github.com/meteoric/ionicons-sass'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('stevezhu:sass@1.3.2');
+
   api.addFiles('sass_include_paths.json', 'server');
 
-  api.addFiles('ionicons-fonts/ionicons.eot', 'client');
-  api.addFiles('ionicons-fonts/ionicons.svg', 'client');
-  api.addFiles('ionicons-fonts/ionicons.ttf', 'client');
-  api.addFiles('ionicons-fonts/ionicons.woff', 'client');
+  api.addFiles([
+    'ionicons-fonts/ionicons.eot',
+    'ionicons-fonts/ionicons.svg',
+    'ionicons-fonts/ionicons.ttf',
+    'ionicons-fonts/ionicons.woff'
+  ], 'client');
+
+
+  api.addFiles([
+    'ionicons-sass/_ionicons-animation.scss',
+    'ionicons-sass/_ionicons-font.scss',
+    'ionicons-sass/_ionicons-icons.scss',
+    'ionicons-sass/_ionicons-variables.scss'
+  ], 'client');
+
+  api.addFiles('_ionicons.scss', 'client');
 });
