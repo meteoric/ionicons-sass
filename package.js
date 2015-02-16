1e98ba0d1b50223be20f1e58a6bafe27780cb202
Nick Wientge <nick@exygeninteractive.com>
2015-02-15 18:42:51 -0800

    upgrade fourseven:scss depenendcy, publish new version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'meteoric:ionicons-sass',
   summary: "Ionic's Ionicons library in SASS and bundled for Meteor.",
-  version: '0.1.5',
+  version: '0.1.6',
   git: 'https://github.com/meteoric/ionicons-sass'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use("fourseven:scss@1.0.0", ["server"]);
+  api.use("fourseven:scss@2.0.0", ["server"]);
   api.imply("fourseven:scss", ["server"]);
 
   api.addFiles([
@@ -16,7 +16,6 @@ Package.onUse(function(api) {
     'fonts/ionicons.ttf',
     'fonts/ionicons.woff'
   ], 'client');
-
 
   api.addFiles([
     'stylesheets/_ionicons-animation.scss',
